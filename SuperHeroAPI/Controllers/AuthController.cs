4ab7816b9bad05f271cd8f092e5c23b68bc8c070
DrozdovNikolai <112856511+DrozdovNikolai@users.noreply.github.com>
2025-05-03 15:58:03 +0300

    Update AuthController.cs

--- SuperHeroAPI/Controllers/AuthController.cs
+++ SuperHeroAPI/Controllers/AuthController.cs
@@ -10,6 +10,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuperHeroAPI.Controllers
 {
